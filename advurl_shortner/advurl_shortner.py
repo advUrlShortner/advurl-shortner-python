c9fed09280ece19737ebe9eb73bb9eeed2e0c73b
forseo2k <154700889+forseo2k@users.noreply.github.com>
2023-12-25 14:03:40 +0300

    Update endpoint URLs

--- advurl_shortner/advurl_shortner.py
+++ advurl_shortner/advurl_shortner.py
@@ -10,7 +10,7 @@ def short(url, password=None, second_url=None, ttl=None, weights=None):
         data["ttl"]=ttl
     if weights:
         data["weights"]=weights
-    response = requests.post('http://illi.cfd/api/newurl/', json=data)
+    response = requests.post('https://advurlshortner.top/api/newurl/', json=data)
 
     if response.status_code == 200:
         output = response.json()
@@ -19,7 +19,7 @@ def short(url, password=None, second_url=None, ttl=None, weights=None):
     return output
 
 def stat(url,password):
-    response = requests.post("http://illi.cfd/api/stat/", json= {"short_url":url,"pass":password})
+    response = requests.post("https://advurlshortner.top/api/stat/", json= {"short_url":url,"pass":password})
     if response.status_code == 200:
         output = response.json()
     else:
